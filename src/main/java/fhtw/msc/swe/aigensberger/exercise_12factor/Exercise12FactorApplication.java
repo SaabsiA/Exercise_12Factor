fc8d675d0bb6204a15517ecdcf8a32c2bef27a73
Sabrina Aigensberger <if18n133@technikum-wien.at>
2021-10-26 10:06:09 +0200

    Init Project

---
+++ src/main/java/fhtw/msc/swe/aigensberger/exercise_12factor/Exercise12FactorApplication.java
@@ -0,0 +1,13 @@
+package fhtw.msc.swe.aigensberger.exercise_12factor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise12FactorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exercise12FactorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fhtw/msc/swe/aigensberger/exercise_12factor/Exercise12FactorApplicationTests.java
@@ -0,0 +1,13 @@
+package fhtw.msc.swe.aigensberger.exercise_12factor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercise12FactorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
